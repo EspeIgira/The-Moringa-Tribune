01507996138424337c3074d61a14e26d15a647d1
EspeIgira <Mapapuro2>
2019-03-29 10:52:40 +0200

    serializers created

--- news/models.py
+++ news/models.py
@@ -125,6 +125,13 @@ class ArticleTestClass(TestCase):
         tags.objects.all().delete()
         Article.objects.all().delete()
 
+# Create products Moringa school might be selling------------------------------------------
+
+class MoringaMerch(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+
 #NewsLetter-------------------------------
 
 class NewsLetterRecipients(models.Model):
